00d096e968e1c4af86c4c6e46c7456289fc85ce6
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-07-28 22:06:17 -0300

    fix: deploy fixed screwed up development environment

--- src/database.ts
+++ src/database.ts
@@ -11,7 +11,7 @@ export default async function connect () {
     type: "postgres",
     url: process.env.DATABASE_URL,
     entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
-    ssl: true
+    ssl: process.env.NODE_ENV === 'production'
   });
   await connection.connect();
   return connection;
