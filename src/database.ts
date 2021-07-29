09fdaddc18b73a49da938c27ce7a31d52b63fa2b
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-07-28 21:48:00 -0300

    fix: heroku deploy fix; ssl configurations

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,17 @@
 import { getConnectionManager } from "typeorm";
 
+if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL.indexOf("sslmode=require") === -1) {
+  process.env.DATABASE_URL += "?sslmode=require";
+}
+
 export default async function connect () {
   const connectionManager = await getConnectionManager();
   const connection = connectionManager.create({
     name: "default",
     type: "postgres",
     url: process.env.DATABASE_URL,
-    entities: ["src/entities/*.ts"]
+    entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
+    ssl: true
   });
   await connection.connect();
   return connection;
